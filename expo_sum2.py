f0536eeb5867e428c3e0a3f40afb824ccae388bc
pedromanfroi <37940271+pedromanfroi@users.noreply.github.com>
2020-08-29 22:35:05 -0300

    Add files via upload

--- expo_sum2.py
+++ expo_sum2.py
@@ -2,7 +2,9 @@ def multiply(a, b):
     return sum([a for _ in range(b)])
 
 def expo_sum(x, y):
+    if x < 0 or y < 0:
+        return "Please enter non-negative numbers for both x and y"
     result = 1
     for _ in range(y):
         result = multiply(result, x)
-        return result
+    return result
